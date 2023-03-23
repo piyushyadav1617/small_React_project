7ff07ba371ce2f9b9e28da5bc1b517cd843467b5
Piyush <yadavpiyush222@gmail.com>
2023-03-23 23:12:12 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { ImageCarousel } from './components/imageCarousel/ImageCarousel'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+
+  return (
+    <div >
+
+      <ImageCarousel />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/imageCarousel/ImageCarousel.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import './imageCarousel.css'
+
+export const ImageCarousel = () => {
+    const images = [
+        "https://source.unsplash.com/random/500x500?lion",
+        "https://source.unsplash.com/random/500x500?wolf",
+        "https://source.unsplash.com/random/500x500?cheetah",
+        "https://source.unsplash.com/random/500x500?elephant",
+        "https://source.unsplash.com/random/500x500?horse"
+    ];
+    const [index, setIndex] = useState(0)
+
+    function handleClickRight(e) {
+
+
+        if (index === images.length - 1) {
+            setIndex(0)
+        }
+        else if (index >= 0 && index < images.length) {
+            setIndex(index + 1)
+
+        }
+
+    }
+    function handleClickLeft() {
+
+        if (index === 0) {
+            setIndex(images.length - 1)
+        }
+        else if (index >= 0 && index < images.length) {
+            setIndex(index - 1)
+
+        }
+    }
+
+    return (
+        <div className="container">
+            <h1>Image Carousel</h1>
+            <div className="carousel">
+                <button className="left" onClick={handleClickLeft}>
+                    <img className="icon" src="src\assets\left-arrow.png" alt="left-arrow" />
+                </button>
+                <button className="right" onClick={handleClickRight}>
+                    <img className="icon" src="src\assets\right-arrow.png" alt="right-arrow" />
+                </button>
+
+                <img className="carousel-img" src={images[index]} />
+
+            </div>
+
+        </div>
+    )
+}
